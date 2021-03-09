c810028daabba2c1a7aec814021c555caac0f714
Paola.Quintanilla <74014.linares@gmail.com>
2021-03-09 13:55:01 -0300

    Adding Navigation

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import MyDrawer from './src/navigation/index'
+
+export default function App() {
+  return(
+    <MyDrawer></MyDrawer>
+  )
+}
---
+++ src/navigation/index.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
+
+function HomeScreen(props: any) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{fontSize:20}}>Home Screen</Text>
+      <TouchableOpacity
+        style={{marginTop:20, width:200, height:50, backgroundColor:'ff5204', padding:10, alignItems:"center", borderRadius:5}}
+        onPress={() => props.navigation.navigate('Perfil')}
+      >
+          <Text>Ir a Perfil</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function PerfilScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{fontSize:20}}>Home Screen</Text>
+      </View>
+    );
+  }
+
+// const Stack = createStackNavigator();
+
+// function Navigation() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Perfil" component={PerfilScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+const Drawer = createDrawerNavigator();
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="Perfil" component={PerfilScreen} />
+        </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;
